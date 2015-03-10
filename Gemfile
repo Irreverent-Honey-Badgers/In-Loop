b16436806be1595978be6d41bf0d2ebb1f94279c
Bernz <BernzAir@BernzAir.local>
2015-03-10 16:57:13 -0400

    added gems

--- Gemfile
+++ Gemfile
@@ -20,6 +20,15 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+# Import CSV Files
+gem 'smarter_csv'
+
+# Creating a Database Diagram with Rails-ERD
+gem 'rails-erd'
+
+# Rename your rails application
+gem 'rails-rename', '~> 1.0.0'
+
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310003909) do
+ActiveRecord::Schema.define(version: 20150310202250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150310003909) do
     t.integer  "favorite"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "doctor_office"
   end
 
   add_index "locations", ["doctor_id"], name: "index_locations_on_doctor_id", using: :btree
