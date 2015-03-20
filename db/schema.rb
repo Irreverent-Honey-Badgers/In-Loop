ed8c6ea6fc9a4f8b98d54262930c1456d5099d6e
Bernz <BernzAir@BernzAir.home>
2015-03-19 21:17:31 -0400

    deleting event table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319184320) do
+ActiveRecord::Schema.define(version: 20150320011537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
