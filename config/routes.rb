c4edb44fc67d3cfbc6d0fb022232d9126b2dd947
Bernz <BernzAir@BernzAir.home>
2015-03-19 21:45:45 -0400

    saving changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,50 @@
-# Prefix Verb     URI Pattern                                     Controller#Action
-#          new_doctor_session GET      /doctors/sign_in(.:format)                      devise/sessions#new
-#              doctor_session POST     /doctors/sign_in(.:format)                      devise/sessions#create
-#      destroy_doctor_session DELETE   /doctors/sign_out(.:format)                     devise/sessions#destroy
-#             doctor_password POST     /doctors/password(.:format)                     devise/passwords#create
-#         new_doctor_password GET      /doctors/password/new(.:format)                 devise/passwords#new
-#        edit_doctor_password GET      /doctors/password/edit(.:format)                devise/passwords#edit
-#                             PATCH    /doctors/password(.:format)                     devise/passwords#update
-#                             PUT      /doctors/password(.:format)                     devise/passwords#update
-#  cancel_doctor_registration GET      /doctors/cancel(.:format)                       devise/registrations#cancel
-#         doctor_registration POST     /doctors(.:format)                              devise/registrations#create
-#     new_doctor_registration GET      /doctors/sign_up(.:format)                      devise/registrations#new
-#    edit_doctor_registration GET      /doctors/edit(.:format)                         devise/registrations#edit
-#                             PATCH    /doctors(.:format)                              devise/registrations#update
-#                             PUT      /doctors(.:format)                              devise/registrations#update
-#                             DELETE   /doctors(.:format)                              devise/registrations#destroy
-#         new_patient_session GET      /patients/sign_in(.:format)                     devise/sessions#new
-#             patient_session POST     /patients/sign_in(.:format)                     devise/sessions#create
-#     destroy_patient_session DELETE   /patients/sign_out(.:format)                    devise/sessions#destroy
-#  patient_omniauth_authorize GET|POST /patients/auth/:provider(.:format)              patients#passthru {:provider=>/google_oauth2/}
-#   patient_omniauth_callback GET|POST /patients/auth/:action/callback(.:format)       patients#:action
-#            patient_password POST     /patients/password(.:format)                    devise/passwords#create
-#        new_patient_password GET      /patients/password/new(.:format)                devise/passwords#new
-#       edit_patient_password GET      /patients/password/edit(.:format)               devise/passwords#edit
-#                             PATCH    /patients/password(.:format)                    devise/passwords#update
-#                             PUT      /patients/password(.:format)                    devise/passwords#update
-# cancel_patient_registration GET      /patients/cancel(.:format)                      devise/registrations#cancel
-#        patient_registration POST     /patients(.:format)                             devise/registrations#create
-#    new_patient_registration GET      /patients/sign_up(.:format)                     devise/registrations#new
-#   edit_patient_registration GET      /patients/edit(.:format)                        devise/registrations#edit
-#                             PATCH    /patients(.:format)                             devise/registrations#update
-#                             PUT      /patients(.:format)                             devise/registrations#update
-#                             DELETE   /patients(.:format)                             devise/registrations#destroy
-#                        root GET      /                                               application#index
-#                   locations GET      /locations(.:format)                            locations#index
-#                             POST     /locations(.:format)                            locations#create
-#                new_location GET      /locations/new(.:format)                        locations#new
-#               edit_location GET      /locations/:id/edit(.:format)                   locations#edit
-#                    location GET      /locations/:id(.:format)                        locations#show
-#                             PATCH    /locations/:id(.:format)                        locations#update
-#                             PUT      /locations/:id(.:format)                        locations#update
-#                             DELETE   /locations/:id(.:format)                        locations#destroy
-#                 eta_patient POST     /patients/:id/eta(.:format)                     patients#eta
-#                    patients GET      /patients(.:format)                             patients#index
-#                edit_patient GET      /patients/:id/edit(.:format)                    patients#edit
-#                     patient GET      /patients/:id(.:format)                         patients#show
-#                             PATCH    /patients/:id(.:format)                         patients#update
-#                             PUT      /patients/:id(.:format)                         patients#update
-#                             DELETE   /patients/:id(.:format)                         patients#destroy
-#          get_patient_doctor GET      /doctors/:id/get_patient(.:format)              doctors#get_patient
-#         find_patient_doctor POST     /doctors/:id/find_patient/:patient_id(.:format) doctors#find_patient
-#                     doctors GET      /doctors(.:format)                              doctors#index
-#                             POST     /doctors(.:format)                              doctors#create
-#                  new_doctor GET      /doctors/new(.:format)                          doctors#new
-#                 edit_doctor GET      /doctors/:id/edit(.:format)                     doctors#edit
-#                      doctor GET      /doctors/:id(.:format)                          doctors#show
-#                             PATCH    /doctors/:id(.:format)                          doctors#update
-#                             PUT      /doctors/:id(.:format)                          doctors#update
-#                             DELETE   /doctors/:id(.:format)                          doctors#destroy
+#                      Prefix Verb   URI Pattern                                     Controller#Action
+#          new_doctor_session GET    /doctors/sign_in(.:format)                      devise/sessions#new
+#              doctor_session POST   /doctors/sign_in(.:format)                      devise/sessions#create
+#      destroy_doctor_session DELETE /doctors/sign_out(.:format)                     devise/sessions#destroy
+#             doctor_password POST   /doctors/password(.:format)                     devise/passwords#create
+#         new_doctor_password GET    /doctors/password/new(.:format)                 devise/passwords#new
+#        edit_doctor_password GET    /doctors/password/edit(.:format)                devise/passwords#edit
+#                             PATCH  /doctors/password(.:format)                     devise/passwords#update
+#                             PUT    /doctors/password(.:format)                     devise/passwords#update
+#  cancel_doctor_registration GET    /doctors/cancel(.:format)                       devise/registrations#cancel
+#         doctor_registration POST   /doctors(.:format)                              devise/registrations#create
+#     new_doctor_registration GET    /doctors/sign_up(.:format)                      devise/registrations#new
+#    edit_doctor_registration GET    /doctors/edit(.:format)                         devise/registrations#edit
+#                             PATCH  /doctors(.:format)                              devise/registrations#update
+#                             PUT    /doctors(.:format)                              devise/registrations#update
+#                             DELETE /doctors(.:format)                              devise/registrations#destroy
+#         new_patient_session GET    /patients/sign_in(.:format)                     devise/sessions#new
+#             patient_session POST   /patients/sign_in(.:format)                     devise/sessions#create
+#     destroy_patient_session DELETE /patients/sign_out(.:format)                    devise/sessions#destroy
+#            patient_password POST   /patients/password(.:format)                    devise/passwords#create
+#        new_patient_password GET    /patients/password/new(.:format)                devise/passwords#new
+#       edit_patient_password GET    /patients/password/edit(.:format)               devise/passwords#edit
+#                             PATCH  /patients/password(.:format)                    devise/passwords#update
+#                             PUT    /patients/password(.:format)                    devise/passwords#update
+# cancel_patient_registration GET    /patients/cancel(.:format)                      devise/registrations#cancel
+#        patient_registration POST   /patients(.:format)                             devise/registrations#create
+#    new_patient_registration GET    /patients/sign_up(.:format)                     devise/registrations#new
+#   edit_patient_registration GET    /patients/edit(.:format)                        devise/registrations#edit
+#                             PATCH  /patients(.:format)                             devise/registrations#update
+#                             PUT    /patients(.:format)                             devise/registrations#update
+#                             DELETE /patients(.:format)                             devise/registrations#destroy
+#                             GET    /patient/auth/:provider/callback(.:format)      authentications#google_oauth2
+#                             GET    /doctor/auth/:provider/callback(.:format)       authentications#google_oauth2
+#        google_oauth2_doctor GET    /doctor/auth/:provider(.:format)                authentications#google_oauth2
+#       google_oauth2_patient GET    /patient/auth/:provider(.:format)               authentications#google_oauth2
+#                        root GET    /                                               application#index
+#         p_omniauth_patients GET    /patients/patient(.:format)                     patients#omniauth
+#                 eta_patient POST   /patients/:id/eta(.:format)                     patients#eta
+#   find_appointments_patient GET    /patients/:id/find_appointments(.:format)       patients#find_appointments
+#                    patients GET    /patients(.:format)                             patients#index
+#                     patient GET    /patients/:id(.:format)                         patients#show
+#          d_omniauth_doctors GET    /doctors/doctor(.:format)                       doctors#omniauth
+#          get_patient_doctor GET    /doctors/:id/get_patient(.:format)              doctors#get_patient
+#     send_appointment_doctor POST   /doctors/:id/send_appointment(.:format)         doctors#send_appointment
+#         find_patient_doctor POST   /doctors/:id/find_patient/:patient_id(.:format) doctors#find_patient
+#                     doctors GET    /doctors(.:format)                              doctors#index
+#                      doctor GET    /doctors/:id(.:format)                          doctors#show
 
 Rails.application.routes.draw do
 
@@ -83,7 +71,7 @@ Rails.application.routes.draw do
     end
     member do
       get '/get_patient' => 'doctors#get_patient', as: 'get_patient'
-      post '/send_appointment' => 'patient#send_appointment',as: 'send_appointment'
+      post '/send_appointment' => 'doctors#send_appointment',as: 'send_appointment'
       post '/find_patient/:patient_id' => 'doctors#find_patient', as: 'find_patient'
     end
   end
