5cd1fd1c5320dc9056768d1d373ac10ad6f6de26
Bernz <BernzAir@BernzAir.local>
2015-04-08 15:17:58 -0400

    styling stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 # Event.create(title: 'test 1', start: '2015-03-23 12:30:00', end_datetime: '2015-03-23 13:30:00', description: 'this should show up on the calendar')
 
-# Appointment.create(patient_id: 1, doctor_id: 1, address: '61 West 23rd Street', city: 'New York', state: 'New York', zip: 10011, title: 'Sore Tooth', start: '2015-03-29 10:00:00', end: '2015-03-29 11:00:00')
-# Appointment.create(patient_id: 1, doctor_id: 2, address: '625 Avenue of the Americas', city: 'New York', state: 'New York', zip: 10010, title: 'Stomach Ache', start: '2015-03-29 10:00:00', end: '2015-03-29 11:00:00')
-# Appointment.create(patient_id: 2, doctor_id: 1, address: '61 West 23rd Street', city: 'New York', state: 'New York', zip: 10011, title: 'Wisdom Teeth', start: '2015-03-29 10:00:00', end: '2015-03-29 11:00:00')
-# Appointment.create(patient_id: 2, doctor_id: 2, address: '625 Avenue of the Americas', city: 'New York', state: 'New York', zip: 10010, title: 'Ulcer', start: '2015-03-29 10:00:00', end: '2015-03-29 11:00:00')
+Appointment.create(patient_id: 1, doctor_id: 1, address: '61 West 23rd Street', city: 'New York', state: 'New York', zip: 10011, title: 'Sore Tooth', start: '2015-04-09 10:00:00', end: '2015-04-09 11:00:00')
+Appointment.create(patient_id: 1, doctor_id: 2, address: '625 Avenue of the Americas', city: 'New York', state: 'New York', zip: 10010, title: 'Stomach Ache', start: '2015-04-09 10:00:00', end: '2015-04-09 11:00:00')
+Appointment.create(patient_id: 2, doctor_id: 1, address: '61 West 23rd Street', city: 'New York', state: 'New York', zip: 10011, title: 'Wisdom Teeth', start: '2015-04-10 10:00:00', end: '2015-04-10 11:00:00')
+Appointment.create(patient_id: 2, doctor_id: 2, address: '625 Avenue of the Americas', city: 'New York', state: 'New York', zip: 10010, title: 'Ulcer', start: '2015-04-10 10:00:00', end: '2015-04-10 11:00:00')
